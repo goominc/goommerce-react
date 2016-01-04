7342b0a59dee06edfc0de10d0f422fa4c98d7c7f
seokgyo <seokgyo@gmail.com>
2016-01-04 14:13:05 +0900

    update webpack output directory. and ignore outputs.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -11,7 +11,7 @@ module.exports = {
     ],
   },
   output: {
-    path: path.join(__dirname, 'server/public'),
+    path: path.join(__dirname, 'dist'),
     filename: '[name].bundle.js',
   },
   plugins: [
