e63a30c3eed5420e0d2a6ca9ea20f50af545c3de
seokgyo <seokgyo@gmail.com>
2016-01-14 13:29:12 +0900

    add navbar. GM-9

--- containers/App.js
+++ containers/App.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 const App = React.createClass({
   propTypes: {
@@ -9,6 +10,19 @@ const App = React.createClass({
     const { children } = this.props;
     return (
       <div className="main container">
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <Link className="navbar-brand" to="/">GOOMMERCE</Link>
+            </div>
+            <div className="collapse navbar-collapse">
+              <ul className="nav navbar-nav navbar-right">
+                <li><Link to="/accounts/signin">LOGIN</Link></li>
+                <li><Link to="/accounts/signup">JOIN</Link></li>
+              </ul>
+           </div>
+          </div>
+        </nav>
         {children}
       </div>
     );
