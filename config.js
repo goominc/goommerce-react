f94a7afd99adb69d75290eea84bc25ad997aa901
seokgyo <seokgyo@gmail.com>
2016-01-20 13:11:30 +0900

    deploy

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const defaultConfig = {
     domain: 'd1v1n6p63zq2b9.cloudfront.net',
   },
   bundle: {
-    version: '76f71c6c75267516e22e580cbb7e1b9e0c2c3b11',
+    version: '6a4b58c1468aa5e58822c698a545998ee0fcd15f',
   },
 };
 
