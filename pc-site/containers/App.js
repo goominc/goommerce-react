ac1d57b725c4861353dee3a34294dac32c3410a6
heekyu <heekyu83@gmail.com>
2016-02-01 11:22:57 +0900

    remove not-using codes

--- pc-site/containers/App.js
+++ pc-site/containers/App.js
@@ -6,38 +6,9 @@ import AppHeader from '../components/AppHeader';
 
 require('../stylesheets/main.scss');
 
-const App = React.createClass({
+export default React.createClass({
   propTypes: {
     children: PropTypes.node,
-    auth: PropTypes.object,
-  },
-  mixins: [LinkedStateMixin, History],
-  getInitialState() {
-    return {};
-  },
-  handleSearch(e) {
-    e.preventDefault();
-    const { query } = this.state;
-    if (query) {
-      this.history.pushState(null, `/search?q=${query}`);
-    }
-  },
-  renderProfile() {
-    const { auth } = this.props;
-    if (auth.bearer) {
-      return (
-        <ul className="nav navbar-nav navbar-right">
-          <li><Link to="/">{auth.email}</Link></li>
-          <li><Link to="/cart">CART</Link></li>
-        </ul>
-      );
-    }
-    return (
-      <ul className="nav navbar-nav navbar-right">
-        <li><Link to="/accounts/signin">LOGIN</Link></li>
-        <li><Link to="/accounts/signup">JOIN</Link></li>
-      </ul>
-    );
   },
   render() {
     const { children } = this.props;
@@ -49,7 +20,3 @@ const App = React.createClass({
     );
   },
 });
-
-export default connect(
-  state => ({ auth: state.auth })
-)(App);
