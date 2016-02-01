55e47acf2b60531b429320892b2e709316ae6634
heekyu <heekyu83@gmail.com>
2016-02-01 18:22:31 +0900

    detach 'CartPage' UI component from 'Cart' container

---
+++ pc-site/components/CartPage.js
@@ -0,0 +1,54 @@
+import React, { PropTypes } from 'react';
+
+export default React.createClass({
+  propTypes: {
+    cart: PropTypes.object,
+    updateCount: PropTypes.func.isRequired,
+    removeProduct: PropTypes.func.isRequired,
+    buy: PropTypes.func.isRequired,
+  },
+  renderVariant(variant) {
+    const { updateCount, removeProduct, buy } = this.props;
+    function handleQuantity(event) {
+      console.log(event.target.value);
+      return updateCount(variant, event.target.value);
+    }
+    return (
+      <li key={variant.sku}>
+        {variant.sku},
+        KRW {variant.price.KRW}
+        <input type="number" name="quantity" min="1" max="100" onChange={handleQuantity} defaultValue={variant.count}/>
+        <button onClick={removeProduct}>Remove</button>
+        <button onClick={buy}>Buy this item</button>
+      </li>
+    );
+  },
+  render() {
+    const { cart, checkout } = this.props;
+    console.log('child');
+    console.log(cart);
+    if (!cart) {
+      return (
+        <div>Error! No Cart</div>
+      );
+    }
+    const variants = cart.productVariants || [];
+    return (
+      <div className="container">
+        <div className="cart-title-box">
+          Your Cart
+        </div>
+        <div className="cart-top-box">
+          <a><span className="cart-continue-shopping-arrow"></span><span className="cart-continue-shopping-text">Continue Shopping</span></a>
+        </div>
+        <div className="cart-seller-box">
+          Seller:
+        </div>
+        <ul>
+          {variants.map(this.renderVariant)}
+        </ul>
+        <button onClick={checkout}>Buy All</button>
+      </div>
+    );
+  },
+});
--- pc-site/containers/Cart.js
+++ pc-site/containers/Cart.js
@@ -4,6 +4,8 @@ import { History } from 'react-router';
 
 import { loadCart, updateCartProduct, deleteCartProduct, createOrder } from '../redux/actions';
 
+import CartPage from '../components/CartPage'
+
 const Cart = React.createClass({
   propTypes: {
     cart: PropTypes.object,
@@ -16,56 +18,30 @@ const Cart = React.createClass({
   componentDidMount() {
     this.props.loadCart();
   },
-  checkout() {
-    const { cart } = this.props;
-    cart.productVariants = cart.productVariants.map(v => ({ id: v.id, count: v.count }));
-    this.props.createOrder(cart).then(
-      (order) => this.history.pushState(null, `/orders/${order.id}/checkout`)
-    );
-  },
-  renderVariant(variant) {
-    function updateCount(e) {
-      this.props.updateCartProduct(variant.id, e.target.value);
+  render() {
+    const { cart, updateCartProduct, deleteCartProduct, createOrder } = this.props;
+    function updateCount(variant, value) {
+      updateCartProduct(variant.id, value);
     }
-    function remove() {
-      this.props.deleteCartProduct(variant.id);
+    function removeProduct(variant) {
+      deleteCartProduct(variant.id);
     }
-    function buy() {
-      this.props.createOrder({
-        productVariants: [{ id: variant.id, count: variant.count }],
+    function buy(productVariants) {
+      if (!productVariants instanceof Array) {
+        productVariants = [productVariants];
+      }
+      createOrder({
+        productVariants: productVariants.map((variant) => { return { id: variant.id, count: variant.count } }),
       }).then(
         (order) => this.history.pushState(null, `/orders/${order.id}/checkout`)
       );
     }
     return (
-      <li key={variant.sku}>
-        {variant.sku},
-        KRW {variant.price.KRW}
-        <input type="number" name="quantity" min="1" max="100" onChange={updateCount.bind(this)} defaultValue={variant.count}/>
-        <button onClick={remove.bind(this)}>Remove</button>
-        <button onClick={buy.bind(this)}>Buy this item</button>
-      </li>
-    );
-  },
-  render() {
-    const { cart } = this.props;
-    const variants = cart.productVariants || [];
-    return (
-      <div className="container">
-        <div className="cart-title-box">
-          Your Cart
-        </div>
-        <div className="cart-top-box">
-          <a><span className="cart-continue-shopping-arrow"></span> <span className="cart-continue-shopping-text">Continue Shopping</span></a>
-        </div>
-        <div className="cart-seller-box">
-          Seller:
-        </div>
-        <ul>
-          {variants.map(this.renderVariant)}
-        </ul>
-        <button onClick={this.checkout}>Buy All</button>
-      </div>
+      <CartPage cart={cart}
+                updateCount={updateCount}
+                removeProduct={removeProduct}
+                buy={buy}
+      />
     );
   },
 });
