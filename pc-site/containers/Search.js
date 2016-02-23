28025e3b8fd2d5ef6b23d1ce0b60792627346132
seokgyo <seokgyo@gmail.com>
2016-02-23 17:35:27 +0900

    search page.

--- pc-site/containers/Search.js
+++ pc-site/containers/Search.js
@@ -1,44 +1,14 @@
 import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { isEqual } from 'lodash';
 
-import ProductThumbnail from '../components/ProductThumbnail';
+import ProductList from './ProductList';
 
-import { ApiAction } from '../redux/actions';
-const { searchProducts } = ApiAction;
-
-const Search = React.createClass({
+export default React.createClass({
   propTypes: {
-    query: PropTypes.object,
-    searchProducts: PropTypes.func.isRequired,
-  },
-  getInitialState() {
-    return {};
-  },
-  componentDidMount() {
-    this.doSearch(this.props);
-  },
-  componentWillReceiveProps(nextProps) {
-    if (!isEqual(this.props.query, nextProps.query)) {
-      this.doSearch(nextProps);
-    }
-  },
-  doSearch(props) {
-    props.searchProducts(props.query).then(res => this.setState({
-      products: res.products,
-    }));
+    location: PropTypes.object.isRequired,
   },
   render() {
-    const { products = [] } = this.state;
     return (
-      <div className="row">
-        {products.map(product => <ProductThumbnail key={product.id} product={product}/>)}
-      </div>
+      <ProductList query={this.props.location.query}/>
     );
   },
 });
-
-export default connect(
-  (state, ownProps) => ({ query: ownProps.location.query }),
-  { searchProducts }
-)(Search);
