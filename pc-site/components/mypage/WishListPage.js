a77cc3cb1b14b987b0b9535337e7fed12f059557
heekyu <heekyu83@gmail.com>
2016-03-03 21:08:02 +0900

    add WishListPage

---
+++ pc-site/components/mypage/WishListPage.js
@@ -0,0 +1,36 @@
+// Copyright (C) 2016 Goom Inc. All rights reserved.
+
+import React, { PropTypes } from 'react';
+
+export default React.createClass({
+  propTypes: {
+    wishes: PropTypes.array,
+  },
+  render() {
+    const { wishes } = this.props;
+    if (!wishes) return (<div></div>);
+
+    const renderWish = (wish) => {
+      return (
+        <div key={wish.id} className="wish-item">
+          <div className="img-box">
+            <div className="img-wrap">
+              <img src="http://res.cloudinary.com/linkshops/image/upload/c_limit,h_330,w_220/v1/old/3f8a2769-copy_1.jpg" />
+            </div>
+          </div>
+          <div className="item-info-box">
+            {wish.productId}
+          </div>
+        </div>
+      );
+    };
+    return (
+      <div className="mypage-right-box">
+        <h2>Wish List</h2>
+        <div className="wish-list-box">
+          {wishes.map(renderWish)}
+        </div>
+      </div>
+    );
+  },
+});
