f422488f639945531517fcb7e5410f0c92b8bcce
heekyu <heekyu83@gmail.com>
2016-03-04 15:20:34 +0900

    use form in signup page

--- pc-site/components/user/SignupPage.js
+++ pc-site/components/user/SignupPage.js
@@ -27,32 +27,33 @@ export default React.createClass({
       );
     };
     const renderBody = () => {
-      const clickSignup = () => {
+      const clickSignup = (e) => {
+        e.preventDefault();
         const user = _.pick(this.state, ['email', 'password', 'passwordConfirm']);
         handleSignup(user);
       };
       return (
-        <div className="signup-form-body">
+        <form onSubmit={clickSignup} className="signup-form-body">
           <div className="form-group">
             <div className="field-label">Email <span className="required"> * </span></div>
             <input type="email" placeholder="enter email"
-                   value={this.state.email}
-                   onChange={(e) => this.setState({ email: e.target.value })} />
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })} />
           </div>
           <div className="form-group">
             <div className="field-label">Password <span className="required"> * </span></div>
             <input type="password" placeholder="password"
-                   value={this.state.password}
-                   onChange={(e) => this.setState({ password: e.target.value })} />
+              value={this.state.password}
+              onChange={(e) => this.setState({ password: e.target.value })} />
           </div>
           <div className="form-group">
             <div className="field-label">Password Confirm <span className="required"> * </span></div>
             <input type="password" placeholder="password(again)"
-                   value={this.state.passwordConfirm}
-                   onChange={(e) => this.setState({ passwordConfirm: e.target.value })} />
+              value={this.state.passwordConfirm}
+              onChange={(e) => this.setState({ passwordConfirm: e.target.value })} />
           </div>
-          <button className="next-button" onClick={clickSignup}>Next</button>
-        </div>
+          <button className="next-button" type="submit">Next</button>
+        </form>
       );
     };
     return (
