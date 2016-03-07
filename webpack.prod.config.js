3e62f7dfdea9ff98c4d57130ab3237ec5e11ed1b
seokgyo <seokgyo@gmail.com>
2016-03-08 01:19:13 +0900

    mobile prod config.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
+module.exports = [{
   devtool: 'source-map',
   entry: {
     app: [
@@ -53,4 +53,55 @@ module.exports = {
       path.resolve('desktop-site'),
     ],
   },
-};
+}, {
+  devtool: 'source-map',
+  entry: {
+    app: [
+      './mobile-site/client.js',
+    ],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].mobile.bundle.js',
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
+    // set global vars
+    new webpack.DefinePlugin({
+      'process.env': {
+        // Useful to reduce the size of client-side libraries, e.g. react
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new ExtractTextPlugin('[name].css'),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: [path.join(__dirname, 'mobile-site'), path.join(__dirname, 'commons')],
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+      },
+    ],
+  },
+  resolve: {
+    root: [
+      path.resolve(__dirname),
+      path.resolve('mobile-site'),
+    ],
+  },
+}];
