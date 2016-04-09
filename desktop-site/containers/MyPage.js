2921d0e45af2f176695bc56e29295804704940ce
heekyu <heekyu83@gmail.com>
2016-04-09 18:09:42 +0900

    change i18n key

--- desktop-site/containers/MyPage.js
+++ desktop-site/containers/MyPage.js
@@ -18,8 +18,8 @@ export default React.createClass({
       { key: 'pcMain.myMenu.myLinkshops', menuName: 'mypage' },
       { key: 'pcMain.myMenu.myOrders', menuName: 'my_orders' },
       { key: 'word.wishList', menuName: 'wish_list' },
-      { key: 'pcMain.myMenu.favoriteBrands', menuName: 'favorite_brands' },
-      { key: 'pcMain.myMenu.reorder', menuName: 'reorder' },
+      { key: 'word.favoriteBrand', menuName: 'favorite_brands' },
+      { key: 'word.reorder', menuName: 'reorder' },
     ];
     let menuIndex = 0;
     for (let i = 1; i < menus.length; i++) {
