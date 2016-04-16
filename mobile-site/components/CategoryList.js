ea3bb4595ad13da0365e9629b345e4c4146055a4
lanipark <lanniranni@gmail.com>
2016-04-16 21:45:41 +0900

    m category pg

--- mobile-site/components/CategoryList.js
+++ mobile-site/components/CategoryList.js
@@ -1,13 +1,17 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import { getProductMainImage, getProductMainPrice } from 'commons/utils/productUtil';
+import productUtil from 'commons/utils/productUtil';
 
 export default React.createClass({
   propTypes: {
     categories: PropTypes.object.isRequired,
     currentCategory: PropTypes.object.isRequired,
+    products: PropTypes.array,
   },
   contextTypes: {
     activeLocale: PropTypes.string,
+    activeCurrency: PropTypes.string,
   },
 
   render() {
@@ -34,11 +38,63 @@ export default React.createClass({
       return null;
     };
 
+    const renderProducts = () => {
+      const { products } = this.props;
+      const { activeCurrency } = this.context;
+      if (!products || !products.length) {
+        return null;
+      }
+
+      return products.map((product) => {
+        const image = getProductMainImage(product.topHit || product);
+        const renderImage = () => {
+          if (!image) {
+            return (<img />);
+          }
+          return (<img src={image.url} />);
+
+          /* if (!image.publicId) {
+            return (<img src={image.url} />);
+          }
+          return (
+            <CloudinaryImage publicId={image.publicId}
+              version={image.version}
+              options={ { width: 220, height: 330 } }
+            />
+          ); */
+        };
+        return (
+          <li className="ms-gallery-item" key={product.id}>
+            <div className="ms-gallery-inner">
+              <Link to={`/products/${product.id}`}>
+                <div className="ms-gallery-pic">
+                  {renderImage()}
+                </div>
+                <div className="ms-gallery-info">
+                  <span className="ms-gallery-dprice">
+                    <em>{activeCurrency} {getProductMainPrice(product, activeCurrency)}</em>
+                  </span>
+                </div>
+              </Link>
+            </div>
+          </li>
+          );
+      });
+    };
+
     return (
       <article className="category-container">
         <ul className="cate-list clearfix">
           {renderCategory()}
         </ul>
+        <section className="products-wrap">
+          <div className="recommend-wrap" id="recommend-wrap">
+            <ul className="ms-gallery" id="recommend-list">
+              {renderProducts()}
+            </ul>
+            <div className="loading"></div>
+          </div>
+        </section>
       </article>
     );
   },
--- mobile-site/containers/Category.js
+++ mobile-site/containers/Category.js
@@ -3,30 +3,80 @@ import { connect } from 'react-redux';
 
 import { ApiAction, setHeader } from 'redux/actions';
 const { loadCategories } = ApiAction;
+import { ajaxReturnPromise } from 'commons/redux/util/ajaxUtil';
 
 import CategoryList from 'components/CategoryList';
+
+const fetchSize = 10;
 
 const Category = React.createClass({
   propTypes: {
     categories: PropTypes.object,
     loadCategories: PropTypes.func.isRequired,
+    searchProducts: PropTypes.func.isRequired,
     setHeader: PropTypes.func.isRequired,
     params: PropTypes.object,
     activeLocale: PropTypes.string,
   },
+  getInitialState() {
+    return { currentCount: 0, maxCount: 0 };
+  },
   componentDidMount() {
-    this.props.setHeader(false, true, true, '');
+    this.props.setHeader(true, true, true, '');
     this.props.loadCategories();
+    this.doSearch(this.props);
+
+    $(window).scroll(() => {
+      if ($(window).scrollTop() + window.innerHeight === $(document).height()) {
+        this.doFetch();
+      }
+    });
   },
   componentWillReceiveProps(nextProps) {
-    const { params, categories, activeLocale } = nextProps;
+    const { params } = nextProps;
 
-    if ((params && params.categoryId && categories[params.categoryId])
+    // FIXME cause unexpected behavior
+    /* if ((params && params.categoryId && categories[params.categoryId])
     || (this.props.activeLocale !== activeLocale)) {
       this.props.setHeader(false, true, true, categories[params.categoryId].name[activeLocale]);
     } else {
       this.props.setHeader(true, true, true, '');
+    }*/
+    if (this.props.params.categoryId !== params.categoryId) {
+      this.doSearch(nextProps);
     }
+  },
+  componentWillUnmount() {
+    $(window).unbind('scroll');
+  },
+  doSearch(props) {
+    const { params } = props;
+    this.props.searchProducts({
+      // q: params.query ? params.query : undefined,
+      categoryId: params.categoryId,
+      offset: 0,
+      limit: fetchSize,
+    }).then((res) => {
+      this.setState({ products: res.products, currentCount: res.products.length, maxCount: res.pagination.total });
+    });
+  },
+  doFetch() {
+    const { params } = this.props;
+    const { currentCount, maxCount } = this.state;
+    if (currentCount === maxCount) {
+      return;
+    }
+    $('.recommend-wrap .loading').show();
+    this.props.searchProducts({
+      // q: params.query ? params.query : undefined,
+      categoryId: params.categoryId,
+      offset: currentCount,
+      limit: fetchSize,
+    }).then((res) => {
+      const mergeProducts = this.state.products.concat(res.products);
+      this.setState({ products: mergeProducts, currentCount: mergeProducts.length });
+      $('.recommend-wrap .loading').hide();
+    });
   },
   render() {
     const { params, categories } = this.props;
@@ -38,12 +88,14 @@ const Category = React.createClass({
     }
     currentCategory = currentCategory || {};
     return (
-      <CategoryList categories={categories} currentCategory={currentCategory} />
+      <CategoryList categories={categories} currentCategory={currentCategory} products={this.state.products} />
     );
   },
 });
 
 export default connect(
-  (state) => ({ categories: state.categories, activeLocale: state.i18n.activeLocale }),
+  (state) => ({ categories: state.categories,
+    activeLocale: state.i18n.activeLocale,
+    searchProducts: (query) => ajaxReturnPromise(state.auth, 'get', `/api/v1/products/search?${$.param(query)}`) }),
   { loadCategories, setHeader }
 )(Category);
