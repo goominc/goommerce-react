2ada558782a0549c8542e157580b2351010a63ae
heekyu <heekyu83@gmail.com>
2016-04-17 23:36:57 +0900

    remove 'united kindom', 'newyork'

--- desktop-site/components/user/SignupPage.js
+++ desktop-site/components/user/SignupPage.js
@@ -24,8 +24,10 @@ export default React.createClass({
         { img: `${constants.resourceRoot}/main/country-kor.png`, name: 'Korea', number: '+82' },
         { img: `${constants.resourceRoot}/main/country-china.png`, name: 'China', number: '+86' },
         { img: `${constants.resourceRoot}/main/country-china.png`, name: 'Hongkong', number: '+886' },
+        /*
         { img: `${constants.resourceRoot}/main/country-england.png`, name: 'United Kingdom', number: '+214' },
         { img: `${constants.resourceRoot}/main/country-usa.png`, name: 'Newyork', number: '+615' },
+        */
         { img: `${constants.resourceRoot}/main/country-taiwan.png`, name: 'Taiwan', number: '+423' },
       ];
       const clickSignup = (e) => {
