e3fa9683b5eca107be5bad40dda034ef1c9c0a5b
heekyu <heekyu83@gmail.com>
2016-04-19 01:06:13 +0900

    fix favorite brand state change bug

--- commons/redux/reducers.js
+++ commons/redux/reducers.js
@@ -125,8 +125,9 @@ function favoriteBrand(state = {}, action) {
     return Object.assign({}, state, { brandProducts: action.brandProducts });
   } else if (action.type === 'DELETE_FAVORITE_BRAND_PRODUCTS') {
     for (let i = 0; i < state.brandProducts.length; i++) {
-      if (state.brandProducts[i][0].brand.id !== action.brandId) {
-        return { brandProducts: state.brandProducts.splice(i, 1) };
+      if (state.brandProducts[i][0].brand.id === action.brandId) {
+        state.brandProducts.splice(i, 1);
+        return merge({}, { brandProducts: state.brandProducts });
       }
     }
     return state;
