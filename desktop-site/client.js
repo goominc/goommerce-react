91808d5900804bf3d3bd8b8104c117f9f3dd17e6
heekyu <heekyu83@gmail.com>
2016-04-19 17:35:11 +0900

    check if auth is null

--- desktop-site/client.js
+++ desktop-site/client.js
@@ -35,11 +35,13 @@ if (window.gaid) {
   ga.initialize(window.gaid, options);
   history.listen((location) => {
     const auth = store.getState().auth;
-    // 2016. 04. 19. [heekyu] TODO remove common logic
-    for (var i = 0; i < (auth.roles || []).length; i++) { // eslint-disable-line
-      const role = auth.roles[i];
-      if (role.type === 'admin') {
-        return;
+    if (auth) {
+      // 2016. 04. 19. [heekyu] TODO remove common logic
+      for (var i = 0; i < (auth.roles || []).length; i++) { // eslint-disable-line
+        const role = auth.roles[i];
+        if (role.type === 'admin') {
+          return;
+        }
       }
     }
     ga.pageview(location.pathname);
