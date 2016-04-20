0105ea9215e6c974beba4175a24dd2a074bd6032
heekyu <heekyu83@gmail.com>
2016-04-21 08:15:23 +0900

    signup footer link

--- desktop-site/components/user/SignupFooter.js
+++ desktop-site/components/user/SignupFooter.js
@@ -8,10 +8,10 @@ export default React.createClass({
     return (
       <div>
         <div className="signup-footer-link-line">
-          <Link className="item" to="/">회사소개</Link>
+          <Link className="item" to="/service/info/why">회사소개</Link>
           <Link className="item" to="/user/terms">이용약관</Link>
           <Link className="item" to="/user/policies">개인정보 취급방침</Link>
-          <Link className="item" to="/">제휴안내</Link>
+          <Link className="item" to="/service/info/signup">제휴안내</Link>
         </div>
         <div className="signup-footer-copywrite">
           APRIL INC.    2016 LINKSHOPS.COM. ALL RIGHTS RESERVED.
