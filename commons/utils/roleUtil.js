598876247fef53a3f2c0bcc632ffc2fac3a14ae5
heekyu <heekyu83@gmail.com>
2016-04-23 14:51:45 +0900

    update roleUtil

--- commons/utils/roleUtil.js
+++ commons/utils/roleUtil.js
@@ -1,20 +1,45 @@
 // Copyright (C) 2016 Goom Inc. All rights reserved.
 
-const message = '바이어 인증 이후에 서비스 이용하실 수 있습니다';
-exports.checkRole = (nextState, replaceState, auth, onNotLogin, onNotRole) => {
+const hasBuyerRole = (auth) => {
   if (!auth || !auth.id) {
-    onNotLogin();
-    return;
+    return false;
   }
   const roles = auth.roles || [];
   for (let i = 0; i < roles.length; i++) {
     if (roles[i].type === 'admin' || roles[i].type === 'buyer' || roles[i].type === 'bigBuyer') {
-      return;
+      return true;
     }
   }
+  return false;
+};
 
-  window.alert(message);
-  onNotRole();
+exports.hasBuyerRole = hasBuyerRole;
+
+const message = '바이어 인증 이후에 서비스 이용하실 수 있습니다';
+exports.checkRoleOnEnter = (nextState, replaceState, auth, onNotLogin, onNotRole) => {
+  if (!auth || !auth.id) {
+    onNotLogin();
+    return;
+  }
+  if (!hasBuyerRole(auth)) {
+    window.alert(message);
+    onNotRole();
+  }
+};
+
+exports.hasRole = (auth, roleTypes) => {
+  if (!(roleTypes instanceof Array)) {
+    roleTypes = [roleTypes];
+  }
+  for (let i = 0; i < (auth.roles || []).length; i++) {
+    const role = auth.roles[i];
+    for (let j = 0; j < roleTypes.length; j++) {
+      if (role.type === roleTypes[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 exports.getBrandIdIfSeller = (auth) => {
