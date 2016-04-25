5187e64a791c5f5018427e469a222cb672e5d09f
heekyu <heekyu83@gmail.com>
2016-04-26 08:46:25 +0900

    Case 128 - click checkbox by javascript since htmlFor does not work in IE8

--- commons/utils/ieCompat.js
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2016 Goom Inc. All rights reserved.
-
-// http://nuli.navercorp.com/sharing/blog/post/792
-exports.onCheckboxLabel = (elem) => {
-  if (navigator.appVersion.indexOf('MSIE') !== -1) {
-    elem.click();
-  }
-};
--- desktop-site/components/user/SignupStep1.js
+++ desktop-site/components/user/SignupStep1.js
@@ -26,7 +26,7 @@ export default React.createClass({
         <div className="signup-terms-section">
           <div className="title">
             <input id="terms_title" type="checkbox" ref="terms" />
-            <label htmlFor="terms_title" onClick={() => ieCompat.onCheckboxLabel($('#terms_title'))}></label>
+            <label onClick={() => $('#terms_title').click()}></label>
             <span style={({ marginLeft: '15px' })}>이용약관 동의</span>
           </div>
           <div className="content-box">
@@ -36,7 +36,7 @@ export default React.createClass({
         <div className="signup-terms-section">
           <div className="title">
             <input id="policies_title" type="checkbox" ref="policies" />
-            <label htmlFor="policies_title" onClick={() => ieCompat.onCheckboxLabel($('#policies_title'))}></label>
+            <label onClick={() => $('#policies_title').click()}></label>
             <span style={({ marginLeft: '15px' })}>개인정보 수집방침 동의</span>
           </div>
           <div className="content-box">
