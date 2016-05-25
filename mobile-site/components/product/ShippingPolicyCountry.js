f2672abf5e094566ab6780553f3ec5d08862c036
heekyu <heekyu83@gmail.com>
2016-05-25 12:05:50 +0900

    update contents

--- mobile-site/components/product/ShippingPolicyCountry.js
+++ mobile-site/components/product/ShippingPolicyCountry.js
@@ -24,12 +24,18 @@ export default React.createClass({
           <div className="content">
             국가 별 통관 및 배송 정책에 따라 kg당 배송비가 상이하게 책정됩니다.
           </div>
-          <div className="shipping-country-title">중국 : 6,500원 (1kg)</div>
+          <div className="shipping-country-title">중국 : 7,000원 (1kg)</div>
           <div className="shipping-country-detail">별도의 비용이 추가되지 않습니다.</div>
-          <div className="shipping-country-title">대만 : 2,500원 (1kg)</div>
-          <div className="shipping-country-detail">통관 완료 후, 현지 관세/부가세/운송료는 별도 납부입니다.</div>
-          <div className="shipping-country-title">홍콩 : 2,700원 (1kg)</div>
-          <div className="shipping-country-detail">별도의 비용이 추가되지 않습니다.</div>
+          <div className="shipping-country-title">대만 : 2,800원 (1kg)</div>
+          <div className="shipping-country-detail">통관 완료 후, 현지 관세/부가세/운송료는 별도 납부하셔야 합니다.</div>
+          <div className="shipping-country-title">홍콩 : 3,000원 (1kg)</div>
+          <div className="shipping-country-detail">
+            별도의 비용이 추가되지 않습니다.<br />
+            * 대만, 홍콩의 경우 박스 포장비용이 별도 발생하며 선결제 배송비에서 차감합니다.<br />
+            주문 상품의 부피에 따라 박스는 중/대 사이즈로 포장이되며 사이즈 별 포장 비용은 다음과 같습니다.<br />
+            - 중 사이즈: 3,000원<br />
+            - 대 사이즈: 5,000원<br />
+          </div>
           <div className="shipping-country-title">그외 국가 : 고객센터(02-2272-1122)로 문의주시면 자세히 안내 드리겠습니다.</div>
           <div className="content-center">
             배송비 책정 기준은 배송정책 변경에 따라<br />
