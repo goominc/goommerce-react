bc75f5dc347d1a4df7664f58895c0d5963e52ea6
seokgyo <seokgyo@gmail.com>
2016-05-25 18:08:42 +0900

    현금 영수증.

--- desktop-site/components/checkout/CheckoutPage.js
+++ desktop-site/components/checkout/CheckoutPage.js
@@ -149,6 +149,7 @@ export default React.createClass({
           <input type="hidden" name="returnUrl" ref="returnUrl" />
           <input type="hidden" name="mKey" ref="mKey" />
           <input type="hidden" name="quotabase" value="2:3:4:5:6:7:8:9:10:11:12" />
+          <input type="hidden" name="acceptmethod" value="va_receipt" ref="acceptmethod" />
           <button type="submit" className="cart-button-order" onClick={handleCheckout}>
             {i18n.get('pcPayment.placeOrder')}
           </button>
