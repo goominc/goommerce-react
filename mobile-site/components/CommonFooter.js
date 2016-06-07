d7b02de1da2656806f2eef45132feab32e275ef5
heekyu <heekyu83@gmail.com>
2016-06-07 18:41:19 +0900

    m - footer i18n

--- mobile-site/components/CommonFooter.js
+++ mobile-site/components/CommonFooter.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import i18n from 'commons/utils/i18n';
+
 export default React.createClass({
   render() {
     return (
@@ -7,23 +9,23 @@ export default React.createClass({
         <div className="footer-linkshops">
           <div className="footer-info-box">
             <div className="info-gray">
-              상호명 - ㈜ 에이프릴<br />
-              대표자 - 서경미<br />
-              주소 - 서울시 중구 퇴계로 324 성우빌딩 6층<br />
-              사업자등록번호 - 201-86-27310<br />
-              통신판매업신고 - 서울중구 60887호<br />
-              개인정보관리책임자 - 오영지
+              {i18n.get('mFooter.company1')}<br />
+              {i18n.get('mFooter.company2')}<br />
+              {i18n.get('mFooter.company3')}<br />
+              {i18n.get('mFooter.company4')}<br />
+              {i18n.get('mFooter.company5')}<br />
+              {i18n.get('mFooter.company6')}
             </div>
             <div className="info-black">
-              <div className="info-title">고객센터 안내</div>
-              평일 - 오전 10:00 ~ 오후 18:00<br />
-              점심시간 - 오전 12:30 ~ 오후 13:30<br />
-              주말, 공휴일 제외(한국기준)<br /><br />
-              Tel - 02-2272-1122<br />
-              Fax - 02-2233-5911<br />
-              E-mail - cs@linkshops.com<br />
-              구매회원 상담 카카오톡 ID - linkshops<br />
-              판매회원 상담 카카오톡 ID - linkshops_brand<br />
+              <div className="info-title">{i18n.get('mFooter.customerCenterTitle')}</div>
+              {i18n.get('mFooter.customerCenter1')}<br />
+              {i18n.get('mFooter.customerCenter2')}<br />
+              {i18n.get('mFooter.customerCenter3')}<br /><br />
+              {i18n.get('mFooter.customerCenter4')}<br />
+              {i18n.get('mFooter.customerCenter5')}<br />
+              {i18n.get('mFooter.customerCenter6')}<br />
+              {i18n.get('mFooter.customerCenter7')}<br />
+              {i18n.get('mFooter.customerCenter8')}<br />
             </div>
           </div>
 
