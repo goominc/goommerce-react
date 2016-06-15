41a37d60bad2ece21ae4eefc3bbd9808a55f769e
heekyu <heekyu83@gmail.com>
2016-06-15 17:28:17 +0900

    m - change service info UI when change locale

--- mobile-site/containers/ServiceInfoContainer.js
+++ mobile-site/containers/ServiceInfoContainer.js
@@ -12,25 +12,37 @@ import SignupInfo from 'components/info/SignupInfo';
 import OrderInfo from 'components/info/OrderInfo';
 import CustomerCenter from 'components/info/CustomerCenter';
 
+import ServiceInfoCn from 'components/info/ServiceInfoCn';
+import SignupInfoCn from 'components/info/SignupInfoCn';
+import OrderInfoCn from 'components/info/OrderInfoCn';
+import CustomerCenterCn from 'components/info/CustomerCenterCn';
+
+import i18n from 'commons/utils/i18n';
+
 const ServiceInfoContainer = React.createClass({
   propTypes: {
     setHeader: PropTypes.func,
+  },
+  contextTypes: {
+    activeLocale: PropTypes.string,
   },
   componentDidMount() {
     this.props.setHeader({
       showLogo: false,
       showSearch: true,
       showCart: true,
-      titleText: '이용안내',
+      titleI18NKey: 'mServiceInfo.title',
     });
   },
   render() {
+    const { activeLocale } = this.context;
     const currentTab = _.get(this.props, 'params.section') || 'service_info';
+    const isChinaLocale = activeLocale === 'zh-cn' || activeLocale === 'zh-tw';
     const tabs = [
-      { name: '서비스소개', key: 'service_info', contents: <ServiceInfo /> },
-      { name: '회원가입', key: 'signup_info', contents: <SignupInfo /> },
-      { name: '주문배송', key: 'order_info', contents: <OrderInfo /> },
-      { name: '고객센터', key: 'customer_center', contents: <CustomerCenter /> },
+      { name: i18n.get('mServiceInfo.infoTitle'), key: 'service_info', contents: isChinaLocale ? <ServiceInfoCn /> : <ServiceInfo /> },
+      { name: i18n.get('mServiceInfo.signupTitle'), key: 'signup_info', contents: isChinaLocale ? <SignupInfoCn /> : <SignupInfo /> },
+      { name: i18n.get('mServiceInfo.orderTitle'), key: 'order_info', contents: isChinaLocale ? <OrderInfoCn /> : <OrderInfo /> },
+      { name: i18n.get('mServiceInfo.customerCenterTitle'), key: 'customer_center', contents: isChinaLocale ? <CustomerCenterCn /> : <CustomerCenter /> },
     ];
     let contents = '';
     tabs.forEach((tab) => {
@@ -40,6 +52,7 @@ const ServiceInfoContainer = React.createClass({
     });
     const renderTab = (tab) => (
       <Link
+        key={tab.key}
         className={`item ${tab.key === currentTab ? 'active' : ''}`}
         to={`/service/info/${tab.key}`}
       >
@@ -58,6 +71,6 @@ const ServiceInfoContainer = React.createClass({
 });
 
 export default connect(
-  undefined,
+  (state, ownProps) => ({}),
   { setHeader }
 )(ServiceInfoContainer);
