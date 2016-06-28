6ad552525221cd656be71ac27113417c3883faba
heekyu <heekyu83@gmail.com>
2016-06-28 18:30:28 +0900

    fix crash when address info does not exist

--- mobile-site/components/order/OrderShippingInfo.js
+++ mobile-site/components/order/OrderShippingInfo.js
@@ -1,4 +1,8 @@
+// Copyright (C) 2016 Goom Inc. All rights reserved.
+
 import React, { PropTypes } from 'react';
+import _ from 'lodash';
+
 import i18n from 'commons/utils/i18n';
 
 export default React.createClass({
@@ -15,19 +19,21 @@ export default React.createClass({
         <div className="box-detail">
           <div className="order-price-black">
             <span className="checkout-item">{i18n.get('pcMypage.fullName')}</span>
-            <span className="info">{order.address.detail.name}</span>
+            <span className="info">{_.get(order, 'address.detail.name', '')}</span>
           </div>
           <div className="order-price-black">
             <span className="checkout-item">{i18n.get('pcMypage.phoneNumber')}</span>
-            <span className="info">{order.address.detail.tel}</span>
+            <span className="info">{_.get(order, 'address.detail.tel', '')}</span>
           </div>
           <div className="order-price-black">
             <span className="checkout-item">{i18n.get('pcMypage.zipCode')}</span>
-            <span className="info">{order.address.detail.postalCode}</span>
+            <span className="info">{_.get(order, 'address.detail.postalCode', '')}</span>
           </div>
           <div className="order-price-black">
             <span className="checkout-item">{i18n.get('pcMypage.address')}</span>
-            <span className="info">{order.address.detail.address.base} {order.address.detail.address.detail}</span>
+            <span className="info">
+              {_.get(order, 'address.detail.address.base', '')} {_.get(order, 'address.detail.address.detail', '')}
+            </span>
           </div>
         </div>
       </div>
